73a08fe2570f863e535f19593a734c5b49d44a8e
StephAO <stephanearocaouellette@gmail.com>
2016-03-13 00:17:51 -0500

    Added comments

--- TestRegulatoryNetworks.py
+++ TestRegulatoryNetworks.py
@@ -3,12 +3,8 @@ from RegulatoryNetworks import Solve, EvaluateExpression
 
 def RunTests():
     print("Running tests")
-##    print(EvaluateExpression(1, [1,0,0], [0,1,0], (-1,2)))
-    Solve(2, [[(0,0), (1,0), (1,1), (0,1)]])
-
-    # Solve()
-
-
+##    print(EvaluateExpression(1, (0,0), (1,0), (-2,1)))
+    Solve(3, [[(0,0,0)],[(0,1,0),(1,0,0)]])
 
 def main():
     RunTests()
